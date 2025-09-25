9b79ad83158f769b046710b747a71cbf3d515453
Emily Gallucci <egallucc@villanova.edu>
2025-09-25 13:48:42 -0400

    updated metrics

--- eval/metrics.py
+++ eval/metrics.py
@@ -1,4 +1,6 @@
 import torch
+from sklearn.metrics import f1_score
+
 @torch.no_grad()
 def top1(model, loader, device):
     model.eval()
@@ -9,3 +11,14 @@ def top1(model, loader, device):
         correct += (pred == yb).sum().item()
         total += yb.numel()
     return correct / total
+
+@torch.no_grad()
+def f1(model, loader, device, average="binary"):
+    model.eval()
+    all_preds, all_labels = [], []
+    for xb, yb in loader:
+        xb, yb = xb.to(device), yb.to(device)
+        pred = model(xb).argmax(1)
+        all_preds.extend(pred.cpu().numpy())
+        all_labels.extend(yb.cpu().numpy())
+    return f1_score(all_labels, all_preds, average=average)
